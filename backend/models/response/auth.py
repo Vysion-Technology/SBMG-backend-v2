f0ed56e717d549b10f31dd1b782e3a9c5d6e21f2
Avanish Gupta <avanish.sde@gmail.com>
2025-10-17 15:52:22 +0530

    refactor: Rearrange and initialize optional fields in PositionHolderResponse model

--- backend/models/response/auth.py
+++ backend/models/response/auth.py
@@ -1,24 +1,28 @@
+"""Response model for position holder."""
+
 from datetime import date
 from typing import Optional
 from pydantic import BaseModel
 
 
 class PositionHolderResponse(BaseModel):
+    """Response model for position holder."""
+
     id: int
     user_id: int
-    role_id: int
-    role_name: str
     first_name: str
-    middle_name: Optional[str]
     last_name: str
     username: str
-    date_of_joining: Optional[date]
-    start_date: Optional[date]
-    end_date: Optional[date]
-    email: Optional[str]
-    district_id: Optional[int]
-    district_name: Optional[str]
-    block_id: Optional[int]
-    block_name: Optional[str]
-    village_id: Optional[int]
-    village_name: Optional[str]
+    middle_name: Optional[str] = None
+    role_id: Optional[int] = None
+    role_name: Optional[str] = None
+    date_of_joining: Optional[date] = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    email: Optional[str] = None
+    district_id: Optional[int] = None
+    district_name: Optional[str] = None
+    block_id: Optional[int] = None
+    block_name: Optional[str] = None
+    village_id: Optional[int] = None
+    village_name: Optional[str] = None
