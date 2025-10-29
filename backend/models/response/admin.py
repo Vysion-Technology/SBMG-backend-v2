9b73d8d135172dff48b2736aa11f2d228c933fd3
Avanish Gupta <avanish.sde@gmail.com>
2025-10-29 07:36:49 +0530

    refactor: Improve docstrings for admin response models and enhance code formatting

--- backend/models/response/admin.py
+++ backend/models/response/admin.py
@@ -1,30 +1,43 @@
+"""Admin Response Models."""
+
 from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Role(BaseModel):
+    """Role information."""
+
     id: int
     name: str
     description: str | None
 
+
 class User(BaseModel):
+    """User information."""
+
     id: int
     username: str
     email: str
     is_active: bool
     role: Role | None
 
+
 class PositionHolder(BaseModel):
+    """Basic position holder information."""
+
     id: int
     user_id: int
     first_name: str
-    middle_name: str | None
     last_name: str
+    middle_name: str | None
     start_date: str | None
     end_date: str | None
 
 
 class UserResponse(BaseModel):
+    """User information response model."""
+
     id: int
     username: str
     email: Optional[str]
@@ -32,6 +45,8 @@ class UserResponse(BaseModel):
 
 
 class RoleResponse(BaseModel):
+    """Role information response model."""
+
     id: int
     name: str
     description: Optional[str]
