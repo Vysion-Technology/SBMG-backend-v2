74f147b5d35462460c780bc63bda8838e58ff830
Avanish Gupta <avanish.sde@gmail.com>
2025-10-29 07:36:57 +0530

    refactor: Reorganize imports and improve formatting in Notice and NoticeMedia models

--- backend/models/database/notice.py
+++ backend/models/database/notice.py
@@ -1,10 +1,12 @@
-from database import Base  # type: ignore
-from typing import Optional
+"""Defines the Notice and NoticeMedia models for the database."""
 from datetime import date
+from typing import Optional
 
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from sqlalchemy import String, Integer, ForeignKey, Date, Text
 
+from database import Base
+from models.database.auth import PositionHolder  # type: ignore
 
 class Notice(Base):  # type: ignore
     """
@@ -14,9 +16,7 @@ class Notice(Base):  # type: ignore
     __tablename__ = "notices"
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True)  # type: ignore
-    sender_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("authority_holder_persons.id"), nullable=False
-    )  # type: ignore
+    sender_id: Mapped[int] = mapped_column(Integer, ForeignKey("authority_holder_persons.id"), nullable=False)  # type: ignore
     receiver_id: Mapped[Optional[int]] = mapped_column(  # type: ignore
         Integer, ForeignKey("authority_holder_persons.id"), nullable=True
     )
@@ -28,6 +28,13 @@ class Notice(Base):  # type: ignore
         "NoticeMedia", back_populates="notice", cascade="all, delete-orphan"
     )
 
+    sender: Mapped["PositionHolder"] = relationship(
+        PositionHolder, foreign_keys=[sender_id]
+    )
+    receiver: Mapped[Optional["PositionHolder"]] = relationship(  # type: ignore
+        PositionHolder, foreign_keys=[receiver_id]
+    )
+
 
 class NoticeMedia(Base):  # type: ignore
     """
@@ -37,9 +44,7 @@ class NoticeMedia(Base):  # type: ignore
     __tablename__ = "notice_media"
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True)  # type: ignore
-    notice_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey(Notice.id), nullable=False
-    )  # type: ignore
+    notice_id: Mapped[int] = mapped_column(Integer, ForeignKey(Notice.id), nullable=False)  # type: ignore
     media_url: Mapped[str] = mapped_column(String, nullable=False)  # type: ignore
 
     notice: Mapped[Notice] = relationship("Notice", back_populates="media")
