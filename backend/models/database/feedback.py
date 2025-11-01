de0b4e9ee997f2f5299ca261c127bdf313801282
Avanish Gupta <avanish.sde@gmail.com>
2025-11-01 12:30:48 +0530

    feat: Add feedback model constraint to ensure at least one user ID is provided

--- backend/models/database/feedback.py
+++ backend/models/database/feedback.py
@@ -3,7 +3,7 @@
 from typing import Optional
 
 
-from sqlalchemy import Integer, ForeignKey
+from sqlalchemy import CheckConstraint, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
@@ -19,3 +19,12 @@ class Feedback(Base):  # type: ignore
     public_user_id: Mapped[Optional[str]] = mapped_column(ForeignKey("public_users.id"), nullable=True)
     rating: Mapped[int] = mapped_column(nullable=False)
     comment: Mapped[Optional[str]] = mapped_column(nullable=True)
+
+    # Constraint that at least one of auth_user_id or public_user_id must be provided
+
+    __table_args__ = (
+        CheckConstraint(
+            (auth_user_id.isnot(None)) | (public_user_id.isnot(None)),
+            name="auth_or_public_user_constraint",
+        ),
+    )
